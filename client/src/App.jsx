a779d6f07f7c689bd28635d65f0e32b661c14078
Suryansh Singh <suryansh.rajs96@gmail.com>
2024-12-05 13:24:59 +0530

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Header } from './components/Header'
+
+import FooterPage  from './components/Footer'
+import Header  from './components/Header'
+
 
 function App() {
 
 
   return (
+    <>
     <Header/>
+    <FooterPage/>
+    </>
+    
   )
 }
 
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,65 @@
+
+"use client";
+
+import { Footer } from "flowbite-react";
+import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+
+function FooterPage() {
+  return (
+    <Footer container>
+      <div className="w-full">
+        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+          <div>
+            <Footer.Brand
+              href="https://flowbite.com"
+              src="./src/assets/images/EasysnapLogo.png"
+              alt="Easysnap Logo"
+              name="Easysnap"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="about" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Easysnap</Footer.Link>
+                <Footer.Link href="#">Tailwind CSS</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Github</Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright href="#" by="Easysnap™" year={2024} />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="https://x.com/Suryansh_rajs" icon={BsTwitter} />
+            <Footer.Icon href="https://github.com/Suryanshrajs" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+          </div>
+        </div>
+        <Footer.Divider />
+        <Footer.LinkGroup  className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center" >
+            <Footer.Link href="#">Made with </Footer.Link>
+        </Footer.LinkGroup>
+        
+      </div>
+    </Footer>
+  );
+}
+
+export default FooterPage;
